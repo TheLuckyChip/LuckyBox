fbdede332e96dbba8cb9f2eeaaf80a35ca0f6050
mefody1971 <prosto_russian@mail.ru>
2018-04-28 16:00:28 +0300

    Fix Arduino.h, ESP8266HTTPUpdateServer lib & LB_Settings.htm

--- touch_interrupt.h
+++ touch_interrupt.h
@@ -4,9 +4,9 @@
 #define _TOUCH_INTERRUPT_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+  #include "Arduino.h"
 #else
-	#include "WProgram.h"
+  #include "WProgram.h"
 #endif
 
 extern void touchInt();
