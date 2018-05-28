031a18f88ec76b9a816f63ab45f7c74282ff5297
TheLuckyChip <phoenim@mail.ru>
2018-05-29 01:12:56 +0300

    Поправлены пины датчика давления

--- user_config.h
+++ user_config.h
@@ -8,8 +8,8 @@
 #define		buzzer			16
 
 // GPIO для I2C
-#define		pSDA			5
-#define		pSCL			4
+#define		pSDA			4
+#define		pSCL			5
 
 // GPIO для DS18B20
 #define		DS_Pin			0
@@ -17,10 +17,10 @@
 // Можно не трогать, программа сама определит количество подключенных датчиков
 #define		DS_Cnt			8
 // Распределение датчиков (нумерация с 0)
-#define		DS_Cube			2 // куб
-#define		DS_Tube			1 // царга
-#define		DS_Out			0 // узел отбора
-#define		DS_Def			3 // дефлегматор
+#define		DS_Cube			0 // куб
+#define		DS_Tube			2 // царга
+#define		DS_Out			3 // узел отбора
+#define		DS_Def			1 // дефлегматор +
 #define		DS_Res1			4 // резерв
 #define		DS_Res2			5 // резерв
 #define		DS_Res3			6 // резерв
@@ -40,9 +40,9 @@
 #define		setRestartWiFi		60
 
 // Какой используем датчик давления
-//#define		Pressure_BMP085
+#define		Pressure_BMP085
 //#define		Pressure_BMP180
-#define		Pressure_BMP280
+//#define		Pressure_BMP280
 //#define		Pressure_BME280
 
 // Вывод отладочной информации в UART
