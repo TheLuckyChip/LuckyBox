e59e4504e9d83dbfb7c80f09ca66f3b8988f0d81
serjrv <serjrv@bk.ru>
2018-12-03 21:48:18 +0400

    Fix version

--- LuckyBox.ino
+++ LuckyBox.ino
@@ -1,6 +1,6 @@
 //   Система автоматики винокура. 
 //   Проект центра открытого проектирования у Счастливчика https://LuckyCenter.ru
-//   Версия 2.1 Release Candidate 7
+//   Версия 2.0 Release Candidate 8
 
 #include "device_view.h"
 #include "pid_config.h"
