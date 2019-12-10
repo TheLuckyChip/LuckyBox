896b4e1a46dbace2dee6defc8de5bbcaafccf9a0
serjrv <serjrv@bk.ru>
2019-12-10 11:30:18 +0400

    fix ethanol sensor sensitivity

--- LuckyBox.ino
+++ LuckyBox.ino
@@ -1,6 +1,6 @@
 //   Система автоматики винокура. 
 //   Проект центра открытого проектирования у Счастливчика https://LuckyCenter.ru
-//   Версия 2.0 Release Candidate 19
+//   Версия 2.0 Release Candidate 19a
 
 #include "device_view.h"
 #include "pid_config.h"
